452ac2c86e9b82b147793a6015ff9ce2417ebb0e
Kushal Kusram <kushal.kusram@ucdenver.edu>
2019-08-23 14:19:46 -0600

    Uploading all files for version control, first push.

---
+++ Assets/UIControl.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIControl : MonoBehaviour {
+    public GameObject openingScreen;
+    private void Start()
+    {
+        openingScreen = GameObject.Find("OpeningScreen");
+    }
+    public void setDestination()
+    {
+        openingScreen.SetActive(false);
+    }
+}

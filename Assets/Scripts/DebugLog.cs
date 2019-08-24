f3b65ac6b26f969ac5f518d2d7d1c940cc7ef46c
KushalBKusram <kushal.kusram@ucdenver.edu>
2019-08-23 19:25:45 -0600

    Added Directional Menu

---
+++ Assets/Scripts/DebugLog.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugLog : MonoBehaviour
+{
+    TextMesh textMesh;
+
+    // Use this for initialization
+    void Start()
+    {
+        textMesh = gameObject.GetComponentInChildren<TextMesh>();
+    }
+
+    void OnEnable()
+    {
+        Application.logMessageReceived += LogMessage;
+    }
+
+    void OnDisable()
+    {
+        Application.logMessageReceived -= LogMessage;
+    }
+
+    public void LogMessage(string message, string stackTrace, LogType type)
+    {
+        if (textMesh.text.Length > 300)
+        {
+            textMesh.text = message + "\n";
+        }
+        else
+        {
+            textMesh.text += message + "\n";
+        }
+    }
+}

0a0cb1478177c433c789f3f3b253cf758c6cb855
KushalBKusram <kushal.kusram@ucdenver.edu>
2019-08-26 14:32:57 -0600

    Working menu change command.

---
+++ Assets/Scripts/UIControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIControl : MonoBehaviour {
+
+    public GameObject openingScreen;
+    public GameObject destinationList;
+
+    private void Start()
+    {
+        openingScreen = GameObject.Find("OpeningScreen");
+        destinationList = GameObject.Find("DestinationList");
+    }
+
+    public void setDestination()
+    {
+        openingScreen.gameObject.active = false;
+        destinationList.gameObject.active = true;
+    }	
+}
---
+++ Assets/Scripts/UIInitializer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIInitializer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GameObject plane = GameObject.Find("Plane");
+        GameObject openingScreen = GameObject.Find("OpeningScreen");
+        GameObject destinationList = GameObject.Find("DestinationList");
+        GameObject journeyScreen = GameObject.Find("JourneyScreen");
+        plane.gameObject.active = true;
+        openingScreen.gameObject.active = true;
+        destinationList.gameObject.active = false;
+        journeyScreen.gameObject.active = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

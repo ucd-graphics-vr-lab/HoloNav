95cf091bf5862f29fca66948fd6d667013264953
KushalBKusram <kushalbkusram@outlook.com>
2020-04-25 11:48:52 -0600

    Updated.

---
+++ Assets/Scripts/DestinationName.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestinationName : MonoBehaviour {
+
+    private string destinyName;
+    public GameObject uiControl;
+
+	// Use this for initialization
+	void Start () {
+        GameObject uiControl = GameObject.Find("MixedRealityCamera");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        UIControl uiScript = uiControl.GetComponent<UIControl>();
+        destinyName = uiScript.destinyName;
+        this.GetComponent<TextMesh>().text = destinyName;
+	}
+}
---
+++ Assets/TurnTheGameOn/Arrow WayPointer/Scripts/Editor/Editor_WaypointController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+namespace TurnTheGameOn.ArrowWaypointer{
+
+	[CustomEditor(typeof(WaypointController))]
+	public class Editor_WaypointController : Editor {
+
+		[MenuItem("Tools/TurnTheGameOn/Waypoint Controller")]
+		static void Create(){
+			GameObject instance = Instantiate(Resources.Load("Waypoint Controller", typeof(GameObject))) as GameObject;
+			instance.name = "Waypoint Controller";
+			instance = null;
+		}
+
+		public override void OnInspectorGUI(){
+
+			WaypointController waypointController = (WaypointController)target;
+			Texture waypointTexture = Resources.Load("Arrow Waypointer") as Texture;
+			GUIStyle inspectorStyle = new GUIStyle(GUI.skin.label);
+			inspectorStyle.fixedWidth = 256;
+			inspectorStyle.fixedHeight = 64;
+			inspectorStyle.margin = new RectOffset((Screen.width-256)/2, (Screen.width-256)/2, 0, 0);
+
+			EditorGUILayout.Space();
+			GUILayout.Label(waypointTexture,inspectorStyle);
+
+			if(!waypointController.player)	EditorGUILayout.HelpBox ("Assign the Player transform to use the waypoints radius value as a waypoint trigger.", MessageType.Info);
+			if(waypointController.configureMode == WaypointController.Switch.On) EditorGUILayout.HelpBox ("Configure Mode is turned on, you must turn it off to complete configuration.", MessageType.Warning);
+
+			EditorGUILayout.Space();
+
+
+			if (GUILayout.Button ("Cleanup Old Wayponts")) {
+				waypointController.CleanUpWaypoints ();
+			}
+
+			EditorGUILayout.Space();
+			EditorGUILayout.Space();
+
+			DrawDefaultInspector ();
+
+			EditorUtility.SetDirty (waypointController);
+
+		}
+		
+	}
+}
